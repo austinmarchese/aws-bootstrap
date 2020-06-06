2da20747fa792179d3d3253ff60827f801b3ca9a
Austin Marchese <amarchese3@gmail.com>
2020-06-06 01:25:16 -0400

    Issue with stop

--- stop-service.sh
+++ stop-service.sh
@@ -3,4 +3,5 @@ source /home/ec2-user/.bash_profile
 [ -d "/home/ec2-user/app/release" ] && \
 cd /home/ec2-user/app/release && \
 npm stop
-for pid in $(ps -ef | grep app.py | awk '{print $2}'); do kill -9 $pid; done
+
+#for pid in $(ps -ef | grep app.py | awk '{print $2}'); do kill -9 $pid; done
