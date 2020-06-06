4023613a62e9cc3643b4bf01f368c8eb380acb99
Austin Marchese <amarchese3@gmail.com>
2020-06-06 08:16:41 -0400

    aa

--- start-service.sh
+++ start-service.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
-cd /home/ec2-user/app/release
-npm run start
+pid_file_name=pid.file
+[ -d "/home/ec2-user/app/release" ] && \
+cd /home/ec2-user/app/release && \
+[ -f "$pid_file_name" ] && kill $(cat ./$pid_file_name)
+
 sudo yum install -y python3 python3-pip
 nohup venv/bin/python3 app.py > ../logs/run.out 2> ../logs/run.err &
+
+nohup venv/bin/python3 app.py &
+echo $! > ./pid.file_2
