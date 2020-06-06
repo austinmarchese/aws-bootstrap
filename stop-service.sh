d32549e3fedcaf7e5bddb1055918ebe3bfc9a685
Austin Marchese <amarchese3@gmail.com>
2020-06-05 23:48:51 -0400

    Adding kill

--- stop-service.sh
+++ stop-service.sh
@@ -3,3 +3,5 @@ source /home/ec2-user/.bash_profile
 [ -d "/home/ec2-user/app/release" ] && \
 cd /home/ec2-user/app/release && \
 npm stop
+
+for pid in $(ps -ef | grep app.py | awk '{print $2}'); do kill -9 $pid; done
