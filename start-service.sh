650d89f4a0ab1982c396be6c23f5cb62312e1d05
Austin Marchese <amarchese3@gmail.com>
2020-06-06 00:28:44 -0400

    CHanging back the nohup

--- start-service.sh
+++ start-service.sh
@@ -3,4 +3,4 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 npm run start
 sudo yum install -y python3 python3-pip
-nohup venv/bin/python3 app.py > ../logs/run.out 2> ../logs/run.err &
+nohup venv/bin/python3 app.py
