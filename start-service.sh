371218bffdfe773fbb6a7dc335bdf835bbf62ade
Austin Marchese <amarchese3@gmail.com>
2020-06-10 23:04:47 -0400

    Altered appspec to clean up instance on stop, changing startup script

--- start-service.sh
+++ start-service.sh
@@ -4,4 +4,4 @@ cd /home/ec2-user/app/release
 npm run start
 sudo mv stanza_resources/ ../../
 sudo yum install -y python3 python3-pip
-nohup venv/bin/python3 app.py > ../logs/run.out 2> ../logs/run.err &
+nohup venv/bin/python3 min_.py > ../logs/run.out 2> ../logs/run.err &
