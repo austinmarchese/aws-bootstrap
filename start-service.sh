671912328fe7604f9fcf0c8127d9cfda5751f3db
Austin Marchese <amarchese3@gmail.com>
2020-06-11 11:06:52 -0400

    Stopping and starting codedeploy agent

--- start-service.sh
+++ start-service.sh
@@ -6,3 +6,6 @@ sudo yum install -y python3 python3-pip
 venv/bin/python3 setup/stanza_setup.py
 #sudo mv stanza_resources/ ../../
 nohup venv/bin/python3 min_.py > ../logs/run.out 2> ../logs/run.err &
+sudo /etc/init.d/codedeploy-agent stop
+sudo /etc/init.d/codedeploy-agent start
+echo 3 > sudo /proc/sys/vm/drop_caches
