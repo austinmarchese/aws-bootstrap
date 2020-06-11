edb64f7be2f53640228bb439943594933a508bc3
Austin Marchese <amarchese3@gmail.com>
2020-06-11 18:51:23 -0400

    Removed Swap from top line

--- start-service.sh
+++ start-service.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
-sudo dd if=/dev/zero of=/root/myswapfile bs=1M count=1024
 nohup sudo bash create-swap.sh  > ../logs/run_swap.out 2> ../logs/run_swap.err &
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
