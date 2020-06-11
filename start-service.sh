5f597dadbc8c3a3e97e8c5c7ce495a0b07a254e2
Austin Marchese <amarchese3@gmail.com>
2020-06-10 21:01:49 -0400

    Moving stanza_resources as part of start script:

--- start-service.sh
+++ start-service.sh
@@ -2,6 +2,6 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 npm run start
-
+sudo mv stanza_resources/ ../../
 sudo yum install -y python3 python3-pip
 nohup venv/bin/python3 app.py > ../logs/run.out 2> ../logs/run.err &
