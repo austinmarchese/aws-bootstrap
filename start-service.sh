d14d84958f8bdbf9412fd8d3b262b30c55d5b9a8
Austin Marchese <amarchese3@gmail.com>
2020-06-10 23:25:48 -0400

    Changing start-service to download stanza in the the startup, removing from build

--- start-service.sh
+++ start-service.sh
@@ -2,6 +2,7 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 npm run start
-sudo mv stanza_resources/ ../../
-sudo yum install -y python3 python3-pip
+venv/bin/python3 setup/stanza_setup.py
+#sudo mv stanza_resources/ ../../
+#sudo yum install -y python3 python3-pip
 nohup venv/bin/python3 min_.py > ../logs/run.out 2> ../logs/run.err &
