73c21f44b65ff94ff110c3a38e29350ba52499fe
Austin Marchese <amarchese3@gmail.com>
2020-06-10 21:18:18 -0400

    Removing Node App

--- start-service.sh
+++ start-service.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
-npm run start
 sudo mv stanza_resources/ ../../
 sudo yum install -y python3 python3-pip
 nohup venv/bin/python3 app.py > ../logs/run.out 2> ../logs/run.err &
