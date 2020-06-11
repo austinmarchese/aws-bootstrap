68226037082caef4a80232859c83c63a585c52ec
Austin Marchese <amarchese3@gmail.com>
2020-06-11 12:30:39 -0400

    Removing Node

--- start-service.sh
+++ start-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
-npm run start
+#npm run start
 sudo yum install -y python3 python3-pip
 venv/bin/python3 setup/stanza_setup.py
 #sudo mv stanza_resources/ ../../
